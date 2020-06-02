7d60630bf23ce08ce98f29954073b97beb037ceb
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-06-02 16:44:59 -0300

    Added controllers to dissociate logic from routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,61 +1,16 @@
-import express, { request, response } from 'express'
-import knex from './database/connection'
+import express from 'express'
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
+// Controller methods: index(all), show(1 element), create, update, delete
 
 const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/items', async (request, response) => {
+routes.get('/items', itemsController.index)
 
-  const items = await knex('items').select('*')
-
-  const serializedItems = items.map(item => {
-
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-    }
-  })
-
-  return response.json(serializedItems)
-})
-
-routes.post('/points', async(request, response) => {
-  const { 
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body
-
-  const trx = await knex.transaction()
-
-  const insertedIds = await trx('points').insert({
-    image: 'fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  })
-
-  const point_id = insertedIds[0]
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id
-    }
-  })
-
-  await trx('point_items').insert(pointItems)
-
-  return response.json({ success: true })
-})
+routes.post('/points', pointsController.create)
 
 export default routes
