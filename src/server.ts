80518c27acc7bdcc6ad1fab44dc5911b1aa91893
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-06-08 10:05:48 -0300

    Added point creation validation

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
-
 import routes from './routes'
+import { errors } from 'celebrate'
 
 const app = express()
 
@@ -11,5 +11,7 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333, () => console.log('Back-end running at #3333 '))
